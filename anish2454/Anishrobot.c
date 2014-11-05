5d57f6888a6d4c4f6a42716c31e45302a6f7d390
Anish Shenoy <shenoy.anish.227@gmail.com>
2014-11-05 17:09:11 -0500

    robotcodeeeee

--- anish2454/Anishrobot.c
+++ anish2454/Anishrobot.c
@@ -16,16 +16,16 @@ task main() {
 		int x2 = joystick.joy1_x2;
 	 	int y2 = joystick.joy1_y2;
 	 	if(y1 > 50){
-	 		motor[Q1] = 100;
-	 		motor[Q2] = 100;
+	 		motor[Q1] = y1;
+	 		motor[Q2] = y1;
 	 	}
 	 	else{
 	 		motor[Q1] = 0;
 	 		motor[Q2] = 0;
 	 	}
 	 	if(y2 < -50){
-	 		motor[Q3] = 100;
-	 		motor[Q4] = 100;
+	 		motor[Q3] = y2;
+	 		motor[Q4] = y2;
 	 	}
 	 	else{
 	 		motor[Q3] = 0;
