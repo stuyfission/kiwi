4d7b3a30a5bfc356f873b7da40d3999f578f4572
Alvin Lin <alvin.lin@stuypulse.com>
2014-11-05 17:18:10 -0800

    fixes

--- anish2454/Anishrobot.c
+++ anish2454/Anishrobot.c
@@ -15,19 +15,19 @@ task main() {
 		int y1 = joystick.joy1_y1;
 		int x2 = joystick.joy1_x2;
 	 	int y2 = joystick.joy1_y2;
-	 	if (y1 > 50) {
-	 		motor[Q1] = 100;
-	 		motor[Q2] = 100;
+	 	if(y1 > 50){
+	 		motor[Q1] = y1;
+	 		motor[Q2] = y1;
 	 	}
-	 	else {
+	 	else{
 	 		motor[Q1] = 0;
 	 		motor[Q2] = 0;
 	 	}
-	 	if (y1 < -50){
-	 		motor[Q3] = 100;
-	 		motor[Q4] = 100;
+	 	if(y2 < -50){
+	 		motor[Q3] = y2;
+	 		motor[Q4] = y2;
 	 	}
-	 	else {
+	 	else{
 	 		motor[Q3] = 0;
 	 		motor[Q4] = 0;
 	 	}
