8098a7376ff1663e6038b4bda0ef7dbf3efc173b
Ryan Siu <rsiu05@gmail.com>
2014-11-10 16:31:26 -0500

    test teleop

--- ryan4848/motor.c
+++ ryan4848/motor.c
@@ -1,19 +1,34 @@
 #include "JoystickDriver.c"
 
 task main () {
-	motor [left] = 100; //-100 to 100
-	getJoystickSettings(joystick);
-	int y1 = joystick.joy1_y1;
-	int y2 = joystick.joy1_y2;
 
-	if (y1 > 50) {
-		//right side forward
-		motor [r1] = 100;
-		motor [r2] = 100;
-	}
+	while true ()
+		getJoystickSettings(joystick);
+		int y1 = joystick.joy1_y1;
+		int y2 = joystick.joy1_y2;
+		int x1 = joystick.joy1_x1;
+		int x2 = joystick.joy1_x2;
 
-	if (y1 < -50) {
-		//
-	}
+		if (y1 > 50) {
+			//right side forward
+			motor [r1] = y1;
+			motor [r2] = y2;
+		}
 
+		else {
+			motor [r1] = 0;
+			motor [r2] = 0;
+		}
 
+		if (y1 < -50) {
+			//left side forward
+			motor [r1] = y1;
+			motor [r2] = y2;
+		}
+
+		else {
+			motor [r1] = 0;
+			motor [r2] = 0;
+		}
+
+}
