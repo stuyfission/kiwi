7eee0ea43b5d9bad86fcfb1a787fe377c43699fe
Alvin Lin <alvin.lin@stuypulse.com>
2014-11-15 02:40:01 -0500

    swag

--- omgimanerd/encryption/multi_file/compile.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-g++ encrypt.cpp -o encrypt
-g++ decrypt.cpp -o decrypt
-ECHO Successfully compiled.
-PAUSE
---
+++ omgimanerd/vectorbot_teleop.c
@@ -0,0 +1,51 @@
+P11636173297041368594891369894356188584210935080535157515070725209006131996567783505819316052887857642186112903465588748485192498
+05&Jqw}uqppu&[xzz(Nksvioq!:20
+/,,
+)*!B~yowwje#h~(Hq|kv!Nir%0ht~jp7motGu{y|x~ttk6gtn1
+(, ktts;60omikvaof{i5onyixh3qv
+%0"puvp>47:98gk|touu0jsp
+(3(Cg{mh!|mopoath!mpr qw{ wflyvz%isl|j6
+'/5
+%xscgqf(jwvgkp)N{iu3$#[:4!N\Qtuwz. #HTPp{pr,""woof5%'(%%ortj1
+*uxconc gtvmqo)J~cy2'"Z6/(Q\Nu|sw-((JTPotrs3! nqpn,!!)%'vtsf,
+)uzhlsc(dqnjno/P}cu5!&Y:.'LW[nzwu4$%owvg,#  #!upne.") !!wtum.
+(qugluh%iqvgkg,Xmu{ws.)T72'"'$#4)(!&($%!((" #  vfutorK4LMvyLtu|wtmokw1
+*uxconc gtvmqo)Unoyuy.'W54)(!&(0%!((" #  #!'!  ugwspsR7JUz}Drtyzvqrgz*
+%pvfoti(dqwgom/Ulrvw{4!Y;0%!((",#  #!'!  "") !!|ju{twJ5IR}Hup|sqlpjz0
++xscpng&jqujlo1Upzwv1!(uvrbS1bD8`1,"") !g{tu|Qjgw2%('%zowuqrRtztit-"xqktSqvt,
++yzbmue%dwvhij(Mruvs, "o}r`T:dJ:d6-#&%('kxqvuTikm|3((!vvpzuyPvvpiu4!uxisMwwr)
+&prdhtb cqpoih)Vt{ww1!#syzfX8aK2a10%('((ccllRkmv3$#()(uswxtsVwtmdl,#pwfnLqqy)
+$q{fnuf%drtkqn-Sq|pt,$%u{zgT4hD8e8.'$#()jbisVnhp|. #  wnvuorPq{mbm5%vxjsMruu1
+*uxconc gtvmqo)Uns|u3"'wu~xgT9gG6`94" # fupuuRiij}S-!)%'(%%!#&%|ZjxxwTvariiyl1
+$r{bmsh"jsqnro)Ymv{p4("suvobT:`C1a45 !!)kywsyMhly[3%&"(!" $%('(|Tg{wuY{cuhdzm1
+$vzelni(eoqfij)Zfrvq.) tstf[8dD4e84'%&"jbekPjn{[4!")!&&'"'$#()|TkzztT|ipddrd,
+$wsagoc)cpoonn0Xjsyu1('xxxw`U3cH9f<4!")!hgjmYmjp}[-&($%!((" #  wTlsvoUvjneb{i0
+
++nsdo{im''PqtvigpLyq~ft7d(
+
+zhur$pirv)/(
+%!qvv {1,#z8- x4.)y3<
+)%~pnqf#.yz|j/"ƒ
+!" $47'Omuu)unk'rvwl|rwoy(sk!|pg moyvupdks"qw uin%nirjqdj3
+('%&imuLo}x|pksTg}uotnu/nr||jis-@
+!(("x4 =#kvzstket.kp‚6f€6@
+!#&%8%C"rp{sxnkr6rp{:`7B
+"'$#€;(>&rs~t|qek1jo|2fy2;
+"") z3)B'rt~twohs5ou{9`{2?
+
+%('(70"_fizvtisw(€qmr(ssm(ooye lg'uhe"lxyturhr{%fsh&fjv{k"i!eevyipv(uj{fynvnk2
+#()(jt|$yizmuhrld#>'30;
+"") jg)-hjx-y4/%D'yntmtjopi1'ƒ
+(!")!&~8"D$3C
+)(!&…
+$%!(qh +abv)€2) >"}hsf|mvti.!~
+&%('%&{9!? 4@
+('((~
+")!&om"/ee{1€3/(@%upzgskolg*'|
+  "") y3)B'8@
+%!#&‚
+('%&kn!*afx0€:1!>)unxluosol2(|
+&($%!(4 @ 0>
+!'! }
+""†
+}
